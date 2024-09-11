5ef5cb933cc1d3113a9f7881cd3f5abd4e36556d
selmaannastasya <130232161+selmaannastasya@users.noreply.github.com>
2024-09-11 20:10:24 +0700

    home.py

--- Tabs/home.py
+++ Tabs/home.py
@@ -8,6 +8,7 @@ def app():
     st.text("(All you need is Ureum and Creatinin Laboratory Check)")
     st.subheader("How to use :")
     st.text("1. Silahkan buka halaman prediksi. Anda bisa menemukannya di bar “Navigation” di sebelah kiri aplikasi.") 
+    st.text("*Untuk pengguna smartphone, bar navigasi dapat dilihat pada tanda panah di sebelah kiri")
     st.text("(Go to the prediction page. You can find it on the “Navigation” bar on the left side of the Application).")
     st.text("2. Isi data yang diminta. Anda bisa terlebih dahulu melakukan check rekam medis untuk mendapatkan data yang dibutuhkan.") 
     st.text("(Fill in the data as requested. You can do a medical checkup first to get the data).")
